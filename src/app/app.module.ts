4bc97611b8b5791df311f136476fa09adff4a762
kapibarra2315 <nikita.nov2016@yandex.ru>
2022-11-08 15:27:17 +0300

    slider projects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { SwiperModule } from 'swiper/angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -30,7 +31,8 @@ import { FooterComponent } from './homepage/shared/components/footer/footer.comp
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SwiperModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/homepage/shared/components/projects/projects.component.ts
+++ src/app/homepage/shared/components/projects/projects.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import SwiperCore, { SwiperOptions ,Navigation, Autoplay} from 'swiper';
 
 @Component({
   selector: 'app-projects',
@@ -6,7 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./projects.component.scss']
 })
 export class ProjectsComponent implements OnInit {
-
+  config: SwiperOptions = {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    navigation: true,
+    pagination: { clickable: true },
+    scrollbar: { draggable: true },
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+  };
   constructor() { }
 
   ngOnInit(): void {
