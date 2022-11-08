bf1fe0d8c629fdc7fe174b7bb1aee8a3e8cc2acb
kapibarra2315 <nikita.nov2016@yandex.ru>
2022-11-08 12:57:57 +0300

    init commit with structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { HeaderComponent } from './homepage/shared/components/header/header.component';
+import { HeroComponent } from './homepage/shared/components/hero/hero.component';
+import { AboutComponent } from './homepage/shared/components/about/about.component';
+import { ProjectsComponent } from './homepage/shared/components/projects/projects.component';
+import { PriceComponent } from './homepage/shared/components/price/price.component';
+import { ServicesComponent } from './homepage/shared/components/services/services.component';
+import { ProcessComponent } from './homepage/shared/components/process/process.component';
+import { ContactsComponent } from './homepage/shared/components/contacts/contacts.component';
+import { FooterComponent } from './homepage/shared/components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent,
+    HeaderComponent,
+    HeroComponent,
+    AboutComponent,
+    ProjectsComponent,
+    PriceComponent,
+    ServicesComponent,
+    ProcessComponent,
+    ContactsComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
