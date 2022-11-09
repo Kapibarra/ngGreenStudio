e2835d788ad5790bdf2190065afebf126fcb983f
kapibarra2315 <nikita.nov2016@yandex.ru>
2022-11-09 12:13:20 +0300

    breakpoints

--- src/app/homepage/shared/components/projects/projects.component.ts
+++ src/app/homepage/shared/components/projects/projects.component.ts
@@ -1,47 +1,52 @@
 import { Projects } from './../../interfaces/projects';
 import { Component, OnInit } from '@angular/core';
-import SwiperCore, { SwiperOptions ,Navigation, Autoplay} from 'swiper';
+import SwiperCore, { SwiperOptions, Navigation, Autoplay } from 'swiper';
 import { ProjectsItems } from '../../interfaces/projectsItems';
-
 
 @Component({
   selector: 'app-projects',
   templateUrl: './projects.component.html',
-  styleUrls: ['./projects.component.scss']
+  styleUrls: ['./projects.component.scss'],
 })
 export class ProjectsComponent implements OnInit {
   config: SwiperOptions = {
-    slidesPerView: 1,
+    slidesPerView: 1.5,
     spaceBetween: 30,
     navigation: true,
     pagination: { clickable: true },
     scrollbar: { draggable: true },
     autoplay: {
       delay: 5000,
-      disableOnInteraction: false
+      disableOnInteraction: false,
+    },
+    breakpoints: {
+      '768': {
+        slidesPerView: 2.5,
+      },
     },
   };
 
-  projects: Projects[] = [ {
-    id: 1,
-    name: 'Сканекс',
-    photos: [{
-id: 1,
-image: 'assets/projects/projects01-1.png'
-    },
+  projects: Projects[] = [
     {
-      id: 2,
-      image: 'assets/projects/projects01-2.png'
-          },
-          {
-            id: 3,
-            image: 'assets/projects/projects01-3.png'
-                },
-  ]
-  }]
-  constructor() { }
+      id: 1,
+      name: 'Сканекс',
+      photos: [
+        {
+          id: 1,
+          image: 'assets/projects/projects01-1.png',
+        },
+        {
+          id: 2,
+          image: 'assets/projects/projects01-2.png',
+        },
+        {
+          id: 3,
+          image: 'assets/projects/projects01-3.png',
+        },
+      ],
+    },
+  ];
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
