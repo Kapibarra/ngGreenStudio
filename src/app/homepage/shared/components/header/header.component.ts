52c53a28fd1481b246e60a70ef35feabd807c857
kapibarra2315 <nikita.nov2016@yandex.ru>
2022-11-10 17:58:25 +0300

    added scroll

--- src/app/homepage/shared/components/header/header.component.ts
+++ src/app/homepage/shared/components/header/header.component.ts
@@ -1,15 +1,16 @@
+import { ViewportScroller } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
+  constructor(private viewportscroller: ViewportScroller) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  onClickScroll(elementId: string): void {
+    this.viewportscroller.scrollToAnchor(elementId);
   }
-
 }
