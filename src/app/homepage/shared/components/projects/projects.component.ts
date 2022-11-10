7580e228ce7704148dea206e280614b9187e8a11
kapibarra2315 <nikita.nov2016@yandex.ru>
2022-11-10 17:01:01 +0300

    fix projects

--- src/app/homepage/shared/components/projects/projects.component.ts
+++ src/app/homepage/shared/components/projects/projects.component.ts
@@ -46,56 +46,80 @@ export class ProjectsComponent implements OnInit {
       ],
     },
     {
-      id: 1,
-      name: 'Сканекс',
+      id: 2,
+      name: 'Технопарк',
       photos: [
         {
           id: 1,
-          image: 'assets/projects/projects01-1.png',
+          image: 'assets/projects/projects02-1.png',
         },
         {
           id: 2,
-          image: 'assets/projects/projects01-2.png',
+          image: 'assets/projects/projects02-2.png',
         },
         {
           id: 3,
-          image: 'assets/projects/projects01-3.png',
+          image: 'assets/projects/projects02-3.png',
+        },
+        {
+          id: 4,
+          image: 'assets/projects/projects02-4.png',
+        },
+        {
+          id: 5,
+          image: 'assets/projects/projects02-5.png',
         },
       ],
     },
     {
-      id: 1,
-      name: 'Сканекс',
+      id: 3,
+      name: 'офис IT',
       photos: [
         {
           id: 1,
-          image: 'assets/projects/projects01-1.png',
+          image: 'assets/projects/projects03-1.png',
         },
         {
           id: 2,
-          image: 'assets/projects/projects01-2.png',
+          image: 'assets/projects/projects03-2.png',
         },
         {
           id: 3,
-          image: 'assets/projects/projects01-3.png',
+          image: 'assets/projects/projects03-3.png',
+        },
+        {
+          id: 4,
+          image: 'assets/projects/projects03-4.png',
         },
       ],
     },
     {
-      id: 1,
-      name: 'Сканекс',
+      id: 4,
+      name: 'Алвилс',
       photos: [
         {
           id: 1,
-          image: 'assets/projects/projects01-1.png',
+          image: 'assets/projects/projects04-1.png',
         },
         {
           id: 2,
-          image: 'assets/projects/projects01-2.png',
+          image: 'assets/projects/projects04-2.png',
         },
         {
           id: 3,
-          image: 'assets/projects/projects01-3.png',
+          image: 'assets/projects/projects04-3.png',
+        },
+        {
+          id: 4,
+          image: 'assets/projects/projects04-4.png',
+        },
+        {
+          id: 5,
+          image: 'assets/projects/projects04-5.png',
+        },
+        {
+          id: 6,
+          image: 'assets/projects/projects04-6.png',
         },
       ],
     },
